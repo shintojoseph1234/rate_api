064d5f8244852daef1dc314a4bdf1bf07c84eb0a
shintojoseph1234 <shintojoseph1234@gmail.com>
2019-06-12 23:53:55 +0530

    changed upload url to upload_price added upload_usd_price POST API

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,6 @@
 # django imports
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
-# local imports
-from api.collections import *
-
-# mongoengine imports
-from rest_framework_mongoengine.serializers import DocumentSerializer, EmbeddedDocumentSerializer
-
-#### Upload API serializer
-class UploadSerializer(DocumentSerializer):
-
-    class Meta:
-        model = Upload
-        fields = '__all__'
 
 
 class UploadPricesSerializer(serializers.Serializer):
@@ -23,6 +10,28 @@ class UploadPricesSerializer(serializers.Serializer):
     date_from           = serializers.DateField()
     date_to             = serializers.DateField()
     price               = serializers.ListField(child=serializers.IntegerField(required=False))
+
+
+    def validate(self, data):
+        """
+        Check that date_from is less than date_to
+        """
+
+        if data['date_from'] > data['date_to']:
+            raise serializers.ValidationError("date_from must be less than or equal to date_to")
+
+        return data
+
+
+
+class UploadUsdPricesSerializer(serializers.Serializer):
+
+    origin_code         = serializers.CharField(max_length=200)
+    destination_code    = serializers.CharField(max_length=200)
+    date_from           = serializers.DateField()
+    date_to             = serializers.DateField()
+    price               = serializers.ListField(child=serializers.IntegerField(required=False))
+    currency_code       = serializers.CharField(max_length=200)
 
 
     def validate(self, data):
